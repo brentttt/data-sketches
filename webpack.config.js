c7d2ee07ebfe17a4889130f698c10e6b99cffb85
Brent <brent.e.hildebrand@gmail.com>
2017-11-27 13:15:22 -0500

    set up app file arch

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/app.js', './src/style/style.scss'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      }, {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader?importLoaders=1'
+        })
+      }, {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'env', 'stage-2']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'style/style.css',
+      allChunks: true
+    })
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
+}
